4d0835d42f35aa173a03da4a5300b550bebe7a14
LIKE-A-G6 <131407111+LIKE-A-G6@users.noreply.github.com>
2025-03-11 22:53:46 +0300

    2 задание

--- main.go
+++ main.go
@@ -2,36 +2,46 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-var task string
-
 type requestBody struct {
 	Task string `json:"task"`
+	Done bool   `json:"done"`
 }
 
-func TaskHandle(w http.ResponseWriter, r *http.Request) {
+func TaskHandler(w http.ResponseWriter, r *http.Request) {
 	var req requestBody
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request", http.StatusBadRequest)
 		return
 	}
-	task = req.Task
+	newTask := Task{Task: req.Task, IsDone: req.Done}
+	if err := DB.Create(&newTask).Error; err != nil {
+		http.Error(w, "Failed to create task", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(newTask)
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, %s\n", task)
+	var tasks []Task
+	if err := DB.Find(&tasks).Error; err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func main() {
+	InitDB()
+	DB.AutoMigrate(&Task{})
+
 	router := mux.NewRouter()
-
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	router.HandleFunc("/api/task", TaskHandle).Methods("POST")
-
+	router.HandleFunc("/api/task", TaskHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
