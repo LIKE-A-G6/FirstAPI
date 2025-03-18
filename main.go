6e7e016c1c4eec2ca1c5e96e0ece93d171dd9928
LIKE-A-G6 <131407111+LIKE-A-G6@users.noreply.github.com>
2025-03-18 21:46:32 +0300

    3 задание

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type requestBody struct {
 	Done bool   `json:"done"`
 }
 
-func TaskHandler(w http.ResponseWriter, r *http.Request) {
+func PostHandler(w http.ResponseWriter, r *http.Request) {
 	var req requestBody
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request", http.StatusBadRequest)
@@ -27,7 +27,7 @@ func TaskHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(newTask)
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
+func GetHandler(w http.ResponseWriter, r *http.Request) {
 	var tasks []Task
 	if err := DB.Find(&tasks).Error; err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
@@ -36,12 +36,64 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var task Task
+	if err := DB.First(&task, id).Error; err != nil {
+		http.Error(w, "Task not found", http.StatusNotFound)
+		return
+	}
+
+	var req requestBody
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	if req.Task != "" {
+		task.Task = req.Task
+	}
+	task.IsDone = req.Done
+
+	if err := DB.Save(&task).Error; err != nil {
+		http.Error(w, "Failed to update task", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var task Task
+	if err := DB.First(&task, id).Error; err != nil {
+		http.Error(w, `{"error": "Task not found"}`, http.StatusNotFound)
+		return
+	}
+
+	if err := DB.Delete(&task).Error; err != nil {
+		http.Error(w, `{"error": "Failed to delete task"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Task successfully deleted"})
+}
+
 func main() {
 	InitDB()
 	DB.AutoMigrate(&Task{})
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	router.HandleFunc("/api/task", TaskHandler).Methods("POST")
+	router.HandleFunc("/api/get", GetHandler).Methods("GET")
+	router.HandleFunc("/api/post", PostHandler).Methods("POST")
+	router.HandleFunc("/api/patch/{id}", PatchHandler).Methods("PATCH")
+	router.HandleFunc("/api/delete/{id}", DeleteHandler).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
